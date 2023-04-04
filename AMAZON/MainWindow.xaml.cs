41b2eb4521313caeababcc131bb7e2d9cc009e3d
Arseny Korkodinov <senchaaa3@gmail.com>
2023-04-04 22:19:20 +0400

    Добавьте файлы проекта.

---
+++ AMAZON/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using AMAZON.Forms;
+using AMAZON.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AMAZON
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Product> ListProduct { get; set; }
+        public Product Product { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            ListProduct = new();
+            DataContext = this;
+        }
+
+        private void btn_add_Click(object sender, RoutedEventArgs e)
+        {
+            Forms.Window_Add add = new Forms.Window_Add();
+            add.ShowDialog();
+            ListProduct.Add(add.Product);
+        }
+
+        private void btn_dlt_Click(object sender, RoutedEventArgs e)
+        {
+            if (Product==null)
+            {
+                return;
+            }
+            Window_Add window = new Window_Add(Product);
+            MessageBoxResult messageBoxResult = MessageBox.Show("Вы уверены?", "Внимание", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (messageBoxResult==MessageBoxResult.Yes)
+            {
+                ListProduct.Remove(window.Product);
+            }
+           
+        }
+
+        private void btn_edt_Click(object sender, RoutedEventArgs e)
+        {
+            if (Product == null)
+            {
+                return;
+            }
+            Window_Add window = new Window_Add(Product);
+            window.ShowDialog();
+        }
+
+        
+    }
+}
---
+++ AMAZON/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace AMAZON.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Options { get; set; }
+        public Image QrCode { get; set; }
+    }
+}
